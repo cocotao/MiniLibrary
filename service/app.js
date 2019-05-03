fb012982786774babf7ad4b8c8283824b3f60bcf
i318440 <coco.tao>
2019-05-03 11:32:41 +0800

    1. Disable session, so verify every time when call end-point. 2. Enable JWT passport verify and Naive passport verify. Naive for /login, JWT for /others. 3. Need to refine code and comment later.

--- service/app.js
+++ service/app.js
@@ -7,20 +7,49 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
 // enale session by koa-session
-const session = require('koa-session')
-app.keys = ['some secret']
-const conf = {
-  encode: json => JSON.stringify(json),
-  decode: str => JSON.parse(str)
-}
-app.use(session(conf, app))
+// const session = require('koa-session')
+// app.keys = ['some secret']
+// const conf = {
+//   encode: json => JSON.stringify(json),
+//   decode: str => JSON.parse(str)
+// }
+// app.use(session(conf, app))
 
 // enale passport for user login authenticated
-const passport = require('./passport/passport')  // require koa-passport from self config file passport.js
+
+
+// 调用use()来为passport新增一个可用的策略
+const passport = require('./passport/passport')
+const jwtStrategy = require('./passport/strategy/jwt')
+const navieStrategy = require('./passport/strategy/naive')
 app.use(passport.initialize())
-app.use(passport.session())
-const naiveStrategy = require('./passport/strategy/naive')
-passport.use(naiveStrategy)   // 调用use()来为passport新增一个可用的策略
+// app.use(passport.session())
+passport.use(navieStrategy)
+passport.use(jwtStrategy)
+// 添加一个koa的中间件，使用naive策略来鉴权。这里暂不使用session
+// app.use(passport.authenticate('naive', {
+//   session: false
+// }))
+
+// 业务代码
+const Router = require('koa-router')
+const router = new Router()
+router.get('/', async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    // ctx.state.user就是鉴权后得到的用户身份
+    ctx.body = 'hello ' + JSON.stringify(ctx.state.user)
+  } else {
+    ctx.throw(401)
+  }
+})
+// router.post('/login', async (ctx) => {
+//   passport.authenticate('naive', {
+//     successRedirect: '/'
+//   })
+// })
+app.use(router.routes())
+  
+
 
 // middlewares
 app.use(bodyparser({
--- service/routes/users.js
+++ service/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const User = require('../controllers/user.controller')
 const passport = require('../passport/passport')
+const secret = 'jwt demo'
 
 
 var jwt = require('jsonwebtoken');
@@ -16,7 +17,7 @@ router.post('/',  async function (ctx, next) {
   }
 })
 
-router.get('/aaa', async (ctx) => {
+router.post('/aaa', async (ctx) => {
   if (ctx.isAuthenticated()) {
     // ctx.state.user就是鉴权后得到的用户身份
     ctx.body = 'hello ' + JSON.stringify(ctx.state.user)
@@ -25,14 +26,98 @@ router.get('/aaa', async (ctx) => {
   }
 })
 
-router.get('/login',
-  passport.authenticate('naive', {
-    successRedirect: '/aaa'
+router.post('/userValidateSuccess',
+  passport.authenticate('jwt', {
+    successRedirect: '/user/bbb',
+    failureFlash: 'Invalid username or password.'
   })
 )
 
+router.post('/bbb', async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    // ctx.state.user就是鉴权后得到的用户身份
+  //  ctx.body = 'hello ' + JSON.stringify(ctx.state.user)
+    ctx.body = {
+      message: '获取token成功',
+      code: 1,
+      token: ctx.header.authorization
+    }
+  } else {
+    ctx.throw(401)
+  }
+})
+
+router.post('/ccc', passport.authenticate('jwt', {}) , async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    // ctx.state.user就是鉴权后得到的用户身份
+  //  ctx.body = 'hello ' + JSON.stringify(ctx.state.user)
+    ctx.body = {
+      message: '获取token成功',
+      code: 1,
+      token: ctx.header.authorization
+    }
+  } else {
+    ctx.throw(401)
+  }
+})
+
+// app.use(router.routes())
+router.post('/login', passport.authenticate('naive', {
+  // successRedirect: '/user/bbb',
+  // failureFlash: 'Invalid username or password.'
+}), async (ctx) => {
+  const user = ctx.request.body
+  if (user && user.username) {
+    let userToken = {
+      name: user.username
+    }
+    const token = jwt.sign(userToken, secret, {
+      expiresIn: '1h' //token签名 有效期为1小时
+    })
+    ctx.header.authorization = token;
+    ctx.body = {
+      message: '获取token成功',
+      code: 1,
+      token
+    }
+    // ctx.response.redirect('/user/userValidateSuccess')
+  } else {
+    ctx.body = {
+      message: '参数错误',
+      code: -1
+    }
+  }
+})
+
+// router.post('/login', 
+//   async (ctx) => {
+//     const user = ctx.request.body
+//     if (user && user.username) {
+//       let userToken = {
+//         name: user.username
+//       }
+//       const token = jwt.sign(userToken, secret, {
+//         expiresIn: '1h' //token签名 有效期为1小时
+//       })
+//       ctx.header.authorization = token;
+//       ctx.body = {
+//         message: '获取token成功',
+//         code: 1,
+//         token
+//       }
+//      ctx.response.redirect('/user/userValidateSuccess')
+//     } else {
+//       ctx.body = {
+//         message: '参数错误',
+//         code: -1
+//       }
+//     }
+//   }
+// )
+
+
 // GET /api/user/logout – clear current JWT token and logout user (Optional)
-router.get('/logout', function (ctx, next) {
+router.post('/logout', function (ctx, next) {
   ctx.logout()
   ctx.body = {
     auth: ctx.isAuthenticated(),

a9f4380896d92ec46319f3651b07dd66d32af8f3
cocotao <242433488@163.com>
2019-05-03 11:36:14 +0800

    1. Disable session, so verify every time when call end-point. 2. Enable (#3)
    JWT passport verify and Naive passport verify. Naive for /login, JWT for
    /others. 3. Need to refine code and comment later.

--- service/passport/passport.js
+++ service/passport/passport.js
@@ -2,15 +2,16 @@ const passport = require('koa-passport')
 
 passport.serializeUser(function (user, done) {
     // 序列化的结果只是一个id
-    done(null, user.id)
+    done(null, user.name)
 })
 
 passport.deserializeUser(async function (str, done) {
     // 根据id恢复用户
     done(null, {
-        id: parseInt(str),
+        username: parseInt(str),
         name: 'user' + str
     })
 })
+
 
 module.exports = passport;
--- service/passport/strategy/jwt.js
+++ service/passport/strategy/jwt.js
@@ -1,44 +1,35 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../../db/models/user')
+const jwt = require('jsonwebtoken')
+const secret = 'jwt demo'
 
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-opts.issuer = 'accounts.examplesoft.com';
-opts.audience = 'yoursite.net';
 
-const jwtStrategy = new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
+// 定义一个验证用户的策略，需要定义name作为标识
+const jwtStrategy = {
+    name: 'jwt',
+    // 策略的主体就是authenticate(req)函数，在成功的时候返回用户身份，失败的时候返回错误
+    authenticate: function (ctx) {
+        const token = ctx.header.authorization // 获取jwt
+        let payload
+        if (token) {
+            payload = jwt.verify(token.split(' ')[1], secret) // // 解密，获取payload
+            this.success(payload)
         } else {
-            return done(null, false);
-            // or you could create a new account
+            this.fail(401)
         }
-    });
-});
+    }
+}
 
-// const jwtStrategy = {
-//     name: 'jwt',
-//     // 策略的主体就是authenticate(req)函数，在成功的时候返回用户身份，失败的时候返回错误
-//     authenticate: function (req) {
-//       let uid = req.query.uid
-//       if (uid) {
-//         // 策略很简单，就是从参数里获取uid，然后组装成一个user
-//         let user = {
-//           id: parseInt(uid),
-//           name: 'user' + uid
-//         }
-//         this.success(user)
-//       } else {
-//         // 如果找不到uid参数，认为鉴权失败
-//         this.fail(401)
-//       }
+
+//   let uid = req.query.uid
+//   if (uid) {
+//     // 策略很简单，就是从参数里获取uid，然后组装成一个user
+//     let user = {
+//       id: parseInt(uid),
+//       name: 'user' + uid
 //     }
-// };
+//     this.success(user)
+//   } else {
+//     // 如果找不到uid参数，认为鉴权失败
+//     this.fail(401)
+//   }
 
 module.exports = jwtStrategy;

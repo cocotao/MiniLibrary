0e9f6bed5646ab0069fe4fdfa52c919af905dd36
i318440 <coco.tao>
2019-05-05 14:22:03 +0800

    Add Userlogin UI.

---
+++ ui/src/components/LoginComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function LoginComponent() {
+  return (
+    <div className="Login">
+      <label>username</label>
+      <input></input>
+      <label>password</label>
+      <input></input>
+    </div>
+  );
+}
+
+export default LoginComponent;
--- ui/src/index.js
+++ ui/src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { orange } from '@material-ui/core/colors'
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: orange[200], // same as '#FFCC80',
+      main: '#FB8C00', // same as orange[600]
+      dark: '#EF6C00',
+      contrastText: 'rgb(0,0,0)'
+    }
+  }
+})
+
+
+ReactDOM.render(<MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

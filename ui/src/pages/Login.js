e284a0e671dbb2a1b499c80dfa206b9c3974fca8
i318440 <coco.tao>
2019-06-18 11:48:07 +0800

    change url to remote

--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -8,24 +8,10 @@ class LoginPage extends React.Component {
         loginInfo : ""
     }
 
-    onLogin = (e) => {
-        var redirectPage = '/WeChatAuthorization';
-        var return_uri = 'http%3A%2F%2F127.0.0.1%2F' + redirectPage;
-        var AppID = 'wx88094ad370bf30a8';
-        var scope = 'snsapi_userinfo';
-        var wechatPath = 'https://open.weixin.qq.com/connect/oauth2/authorize'
-          + '?appid=' + AppID
-          + '&redirect_uri=' + return_uri
-          + '&response_type=code' 
-          + '&scope=' + scope
-          + '&state=STATE#wechat_redirect';
-
-        window.location.href = wechatPath;
-    }
-
     render() {
       var redirectPage = 'WeChatAuthorization';
-        var return_uri = 'http%3A%2F%2F127.0.0.1%2F' + redirectPage;
+        // var return_uri = 'http%3A%2F%2F127.0.0.1%2F' + redirectPage;
+        var return_uri = 'http%3A%2F%2F148.70.236.60%2F' + redirectPage;
         var AppID = 'wx88094ad370bf30a8';
         var scope = 'snsapi_userinfo';
         var wechatPath = 'https://open.weixin.qq.com/connect/oauth2/authorize'
@@ -36,8 +22,8 @@ class LoginPage extends React.Component {
           + '&state=STATE#wechat_redirect';
 
         return (
-            <div >y
-               <button onClick={() => this.onLogin()}>Wechat Login</button>
+            <div >
+               
                 <div> {this.state.loginInfo} </div>
 
                 <a href = {wechatPath}>直接跳转到微信提供的网页</a>

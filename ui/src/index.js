34f4e5643f1f6292c0a6391a51dc7f64a39999f1
i318440 <coco.tao>
2019-06-26 22:23:30 +0800

    update

--- ui/src/index.js
+++ ui/src/index.js
@@ -7,20 +7,15 @@ import 'react-weui/build/packages/react-weui.css';
 
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 
 import MainPage from './pages/MainPage'
 import Login from './pages/Login'
 import WeChatAuthorization from './pages/WeChatAuthorization'
 
-
-// import SignIn from './components/SignIn'
-// import BookList from './components/BookList'
-// import BorrowedList from './components/BorrowedList'
-// import WeUiDemo from './components/weuiDemo'
-// import WeUiTabBar from './components/weuiTabBar'
-
+/*
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 // import { orange } from '@material-ui/core/colors'
 const theme = createMuiTheme({
@@ -33,24 +28,16 @@ const theme = createMuiTheme({
     // }
   }
 })
+*/
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
+  // <MuiThemeProvider theme={theme}>
     <Router>
-        {/* <Route path="/aaa" exact component={SignIn} 
-           render={props => <SignIn {...props} />}
-        />
-        <Route path="/booklist/" component={BookList} />
-        <Route path="/borrowlist/" component={BorrowedList} />
-        <Route path="/weuidemo" component={WeUiDemo} /> */}
-        {/* <Route path="/" component={MainPage} */}
          <Route path="/" component={Login} />
          <Route path="/wechatauthorization" component={WeChatAuthorization} />
          <Route path="/mainpage" component={MainPage} />
-    </Router>
-  </MuiThemeProvider>, document.getElementById('root'));
+    </Router> 
+      , document.getElementById('root'));
+  // </MuiThemeProvider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
